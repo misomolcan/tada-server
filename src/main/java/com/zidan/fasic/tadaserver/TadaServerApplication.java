9e3fb8775b029e5752bdb3fe9598270db164b9e7
miso.molcan@gmail.com <miso.molcan@gmail.com>
2018-05-29 22:01:54 +0200

    uz bavi

---
+++ src/main/java/com/zidan/fasic/tadaserver/TadaServerApplication.java
@@ -0,0 +1,12 @@
+package com.zidan.fasic.tadaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TadaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TadaServerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/zidan/fasic/tadaserver/TadaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zidan.fasic.tadaserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TadaServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
