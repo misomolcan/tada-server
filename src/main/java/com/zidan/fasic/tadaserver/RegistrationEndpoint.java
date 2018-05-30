602a87e2f8900eec7fec7ff0f07dd61d44ec54d6
miso.molcan@gmail.com <miso.molcan@gmail.com>
2018-05-30 13:04:27 +0200

    registration works

--- src/main/java/com/zidan/fasic/tadaserver/RegistrationEndpoint.java
+++ src/main/java/com/zidan/fasic/tadaserver/RegistrationEndpoint.java
@@ -4,23 +4,53 @@ import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
-import search.microsoft.Query;
-import search.microsoft.QueryResponse;
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
 import search.microsoft.Registration;
 import search.microsoft.RegistrationResponse;
 
-import javax.jws.WebMethod;
+import javax.xml.bind.Element;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 @Endpoint
 public class RegistrationEndpoint {
 
     @PayloadRoot(namespace = "urn:Microsoft.Search", localPart = "Registration")
     @ResponsePayload
-    @WebMethod(action = "urn:Microsoft.Search/Registration")
-    public RegistrationResponse processRegistrationRequest(@RequestPayload Registration request) {
+    public RegistrationResponse processRegistration(@RequestPayload Registration request) {
+
         RegistrationResponse response = new RegistrationResponse();
 
-        response.setRegistrationResult("registration success");
+
+        String s = "<ProviderUpdate xmlns=\"urn:Microsoft.Search.Registration.Response\">" +
+                "  <Status>SUCCESS</Status>" +
+                "  <Providers>" +
+                "    <Provider>" +
+                "      <Message>All Your Base Are Belong To Us</Message>" +
+                "      <Id>{C37EE888-D74E-47e5-B113-BA613D87F0B2}</Id>" +
+                "      <Name>CLASSIC FASIC</Name>" +
+                "      <QueryPath>http://192.168.20.170:8080/ws/research.wsdl/" +
+                "        Registration</QueryPath>" +
+                "      <RegistrationPath>>http://192.168.20.170:8080/ws/research.wsdl/" +
+                "        Registration</RegistrationPath>" +
+                "      <Type>SOAP</Type>" +
+                "      <Services>" +
+                "        <Service>" +
+                "          <Id>{8DD063CA-94FC-4514-8D83-3B36B12432BE}</Id>" +
+                "          <Name>FASIC rules you morons</Name>" +
+                "          <Description>TADA service, that will make your days happier </Description>" +
+                "          <Copyright>(C) 2018</Copyright>" +
+                "          <Display>On</Display>" +
+                "          <Category>RESEARCH_GENERAL</Category>" +
+                "        </Service>" +
+                "      </Services>" +
+                "    </Provider>" +
+                "  </Providers>" +
+                "</ProviderUpdate>";
+        response.setRegistrationResult(s);
 
         return response;
     }
--- src/main/java/com/zidan/fasic/tadaserver/WebServiceConfig.java
+++ src/main/java/com/zidan/fasic/tadaserver/WebServiceConfig.java
@@ -39,21 +39,5 @@ public class WebServiceConfig {
         return new SimpleXsdSchema(new ClassPathResource("researchService.xsd"));
     }
 
-//    @Bean(name = "students")
-//    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema studentsSchema) {
-//        DefaultWsdl11Definition definition = new DefaultWsdl11Definition();
-//        definition.setPortTypeName("StudentPort");
-//        definition.setTargetNamespace("http://in28minutes.com/students");
-//        definition.setLocationUri("/ws");
-//        definition.setSchema(studentsSchema);
-//        return definition;
-//    }
-//
-//    @Bean
-//    public XsdSchema studentsSchema() {
-//        return new SimpleXsdSchema(new ClassPathResource("student-details.xsd"));
-//    }
-
-
 }
 
